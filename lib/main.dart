012f059fff9dc2ede26e38b03cac5dc1af70f31b
Jeffer <jeffersondavila1101@gmail.com>
2025-11-23 19:32:51 -0500

    Fix build

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'World Discovery',
+      title: 'Bogota Discovery',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
